77032326ad2cadd96d0e6bc1e620b3fbe68e50a5
phamvanvuhb1999 <58477969+phamvanvuhb1999@users.noreply.github.com>
2020-09-02 17:04:52 +0700

    redefine App component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@
 import React, { Component } from "react";
 import Item from "./components/Item";
 
+//redefine App component to render a list
 class App extends Component {
   constructor() {
     super();
